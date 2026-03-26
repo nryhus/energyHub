52b684909ed2f08797ce458e9b3bdc836e73eb0d
Nazarii Ryhus <superkriper45@gmail.com>
2026-03-26 19:15:04 +0200

    new header

--- front/src/app/components/Header/Header.tsx
+++ front/src/app/components/Header/Header.tsx
@@ -26,9 +26,21 @@ const Header = () => {
 
                         <button className={css.NavbarLink} onClick={() => toggleMenu("available")}>
                             Авто в наявності
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                 stroke-linejoin="round"
+                                 className="lucide lucide-chevron-down-icon lucide-chevron-down">
+                                <path d="m6 9 6 6 6-6"/>
+                            </svg>
                         </button>
                         <button className={css.NavbarLink} onClick={() => toggleMenu("order")}>
                             Авто під замовлення
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                 stroke-linejoin="round"
+                                 className="lucide lucide-chevron-down-icon lucide-chevron-down">
+                                <path d="m6 9 6 6 6-6"/>
+                            </svg>
                         </button>
 
                         <Link href={"/merch-catalog"} className={css.NavbarLink}>Магазин Автозапчастин</Link>
